e765a06bdd3165d6cdb21e59766ee53aa516d54a
HiYUNo <jayhyun1008@gmail.com>
2024-08-23 15:14:22 +0900

    \n replace..?

--- js/index.js
+++ js/index.js
@@ -48,8 +48,8 @@ function wikiParse(text) {
     text = text.replace(/\n/gm, '')
     text = text.replace(/\[\[(.+)\|(.+)\]\]/gm, '[$1](./?d=$2)')
     text = text.replace(/\[\[(.+)\]\]/gm, '[$1](./?d=$1)')
-    markdown = markdown.replace(/\\n\\n/gm, '</p><p>')
-    markdown = markdown.replace(/\\n/gm, '<br>')
+    markdown = markdown.replace(/\\n\\n/gm, '\n\n')
+    markdown = markdown.replace(/\\n/gm, '\n')
     var markdown = marked.parse(text)
     return markdown
 }
